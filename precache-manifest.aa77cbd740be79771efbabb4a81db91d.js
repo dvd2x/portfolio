26021f118cd9978f6a7d9f4a4081a11d707dba26
David Piper <black.god1930@gmail.com>
2019-09-13 16:31:56 -0400

    Updates

---
+++ precache-manifest.aa77cbd740be79771efbabb4a81db91d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3e62d181e804683f1881bf6d7ea57f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "86325e2a12a855b680fc",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "f7b4c6c09028d2c97536",
+    "url": "/portfolio/static/css/main.8e6f5db2.chunk.css"
+  },
+  {
+    "revision": "86325e2a12a855b680fc",
+    "url": "/portfolio/static/js/2.c8592aa6.chunk.js"
+  },
+  {
+    "revision": "f7b4c6c09028d2c97536",
+    "url": "/portfolio/static/js/main.eb6ad73a.chunk.js"
+  },
+  {
+    "revision": "8f45c040d91d5d7ec116",
+    "url": "/portfolio/static/js/runtime~main.9ed6450e.js"
+  },
+  {
+    "revision": "36b69d18a426ba320e899308b119a1f4",
+    "url": "/portfolio/static/media/David1.36b69d18.png"
+  },
+  {
+    "revision": "c201aa6e77914f43d06873a78890e989",
+    "url": "/portfolio/static/media/David2.c201aa6e.png"
+  }
+]);
